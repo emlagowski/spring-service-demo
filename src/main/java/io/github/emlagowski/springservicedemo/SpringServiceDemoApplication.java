68c00ad45d9b413da895514c0bed9210b2af5c8e
Marcin Łagowski <marcinlagowski92@gmail.com>
2020-05-25 15:18:06 +0200

    Initial commit

---
+++ src/main/java/io/github/emlagowski/springservicedemo/SpringServiceDemoApplication.java
@@ -0,0 +1,32 @@
+package io.github.emlagowski.springservicedemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServiceDemoApplication.class, args);
+    }
+
+    @RestController
+    static final class MyController {
+
+        private final String greetings;
+
+        MyController(@Value("${demo.greetings}") String greetings) {
+            this.greetings = greetings;
+        }
+
+        @GetMapping("/")
+        public String helloWorld() {
+            return greetings + " World!\n";
+        }
+
+    }
+
+}
---
+++ src/test/java/io/github/emlagowski/springservicedemo/SpringServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emlagowski.springservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
